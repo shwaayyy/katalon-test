9fdd62235c2a035e8668a42c367cbc4f7d537591
shwaayyy <wahyu@digi-id.id>
2022-11-29 13:20:34 +0700

    new commit

---
+++ Object Repository/Page_Digisign/Page_Digisign/div_wayyyshelbygmail.com                   _ebda70.rs
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_wayyyshelbygmail.com                   _ebda70</name>
+   <tag></tag>
+   <elementGuidId>9aa6f690-c26f-4241-b561-b73d022d8bce</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Pilih Akun Email Anda'])[1]/following::div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>a1f08eac-5c02-43af-a281-d41ba91c5886</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>alert alert-info wrap</value>
+      <webElementGuid>7f6987c3-f945-42ba-ab74-9392e0c6c505</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                           wayyyshelby@gmail.com
+                                                      
+                           Personal
+                                                   </value>
+      <webElementGuid>9d466a90-1131-45bc-85f5-7f2e0e86e950</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;container&quot;]/div[@class=&quot;row justify-content-center align-items-center text-center&quot;]/div[@class=&quot;row justify-content-center align-items-center text-center&quot;]/div[@class=&quot;col-md-5 col-md-offset-4&quot;]/section[@class=&quot;login-form&quot;]/form[1]/a[@class=&quot;dropdown-item&quot;]/div[@class=&quot;alert alert-info wrap&quot;]</value>
+      <webElementGuid>ded54f4f-27fa-49a6-a143-3c31999e36f2</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pilih Akun Email Anda'])[1]/following::div[2]</value>
+      <webElementGuid>98f97d90-0e46-4575-b65e-b7077ab032de</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kembali ke Login'])[1]/preceding::div[1]</value>
+      <webElementGuid>68d3f348-649e-4363-8264-94073bece357</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='wayyyshelby@gmail.com']/parent::*</value>
+      <webElementGuid>9c0103be-1b98-4d69-8043-c1e72bafefd7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[2]/div</value>
+      <webElementGuid>fad45b40-c86b-4ff0-b42f-45bbdfe0a3d3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+                           wayyyshelby@gmail.com
+                                                      
+                           Personal
+                                                   ' or . = '
+                           wayyyshelby@gmail.com
+                                                      
+                           Personal
+                                                   ')]</value>
+      <webElementGuid>f8a51e78-ed9a-4a43-b8e7-31448cfeff7a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
